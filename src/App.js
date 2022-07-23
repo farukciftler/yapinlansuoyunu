9906592cef17ad22984d1f516d0b23137dfe4856
Lidya Kaya <72206348+thezkaya@users.noreply.github.com>
2022-07-23 11:04:58 +0300

    Update
    Join Game menu css &

--- src/App.js
+++ src/App.js
@@ -56,11 +56,11 @@ function App() {
 
   const [page, setPage] = useState(0);
   const [isUserCame, setIsUserCame] = useState(0);
-  const [inputValue, setInputValue] = useState("");
+  // const [inputValue, setInputValue] = useState("");
   const [roomId, setRoomId] = useState("");
   const [isUsernameSet, setIsUsernameSet] = useState(0);
   const [username, setUsername] = useState("");
-
+  const [isGuessSet, setIsGuessSet] = useState(0);
 
   function MainMenu() {
     return (
@@ -77,36 +77,111 @@ function App() {
     )
   }
   function NewGameMenu() {
+    const [username, setUsername] = useState("");
+    const [isCopied, setIsCopied] = useState(false);
+    
+    async function copyTextToClipboard(text) {
+      if ('clipboard' in navigator) {
+        return await navigator.clipboard.writeText(text);
+      } else {
+        return document.executeCommand('copy', true, text);
+      }
+    }
+    const handleCopyClick = () => {
+     
+      copyTextToClipboard(roomId)
+        .then(() => {
+          setIsCopied(true);
+          setTimeout(() => {
+            setIsCopied(false);
+          }, 1500);
+        })
+  
+    }
+
+  
+
 
     return (
       <div className='NewGameMenuDiv'>
-        <Button className='NewGameMenuRoomNumberButton' variant="secondary" style={{ marginTop: 100 }} >
-          <p className='NewGameMenuRoomNumberButtonText'>{roomId}</p>
+        <Button   className='NewGameMenuRoomNumberButton' variant="secondary" style={{ marginTop: 100 }} >
+          <p value={roomId} className='NewGameMenuRoomNumberButtonText'>{roomId}</p>
         </Button>
         <br></br>
-        <Button variant='dark' className='NewGameMenuRoomShareButton'>
-          <p className='NewGameMenuRoomShareButtonText'>SHARE</p>
+        <Button variant='dark'  onClick={handleCopyClick}className='NewGameMenuRoomShareButton' >
+        
+          <p className='NewGameMenuRoomShareButtonText'>{isCopied ? 'Copied!' : 'Share'}</p>
 
         </Button>
         <br></br>
         <br></br>
-        {isUsernameSet == 0 &&
+     
+        
+        {/* {isUsernameSet == 0 && */}
           <form>
-            <input type="text" name="name" className="question" id="nme" required autoComplete="off" onChange={(e) => setUsername(this.e)} />
-            <label htmlFor="nme"><span>What's your name?</span></label>
-            <Button variant='dark' className='NewGameMenuRoomShareButton' onClick={() => [setIsUsernameSet(1)]}>
-              <p className='NewGameMenuRoomShareButtonText'>Enter</p>
+            <input 
+            // type="text" 
+            // name="name"
+            // className='JoinMenuDiv'
+            // id="nme"
+            required autoComplete="off" 
+            placeholder="Enter Your Name"
+            value={username}
+            
+            onChange={(e) => setUsername(e.target.value)}/>
+            <Button variant='dark'  className='EnterBtn' onClick={() => [setIsUsernameSet(1)]}>
+              <p className='EnterBtnText'> Enter </p>
             </Button>
           </form>
-        }
+        {/* } */}
         {isUsernameSet == 1 &&
-          <h2 className='WaitingPlayerText'>Waiting other player!</h2>
+          <h2 className='WaitingPlayerText'>Waiting other player! + {username}</h2>
+          // <GameScreen />
         }
 
       </div>
     )
   }
   function GameScreen() {
+    const [inputValue, setInputValue] = useState('');
+    // const [items, setItems] = useState([]);
+
+    // numpad functions
+    function handleClick(e){
+      if (inputValue.startsWith(0)=== true){
+        //problem
+        alert("the secret code cannot start with 0 ");
+        setInputValue('');
+        return;
+      }
+      setInputValue(inputValue + e.target.value);
+    }
+    function delBtn(){
+      setInputValue(inputValue.substring(0, inputValue.length - 1));
+    }
+    function handleChange(e){
+      
+      setInputValue(e.target.value);
+      if (isNaN(inputValue)==true){
+        setInputValue('');
+        alert("just number!");
+        return;
+      }
+      if (inputValue.startsWith(0)=== true){
+        //problem
+        alert("the secret code cannot start with 0 ");
+        setInputValue('');
+        return;
+      }
+      for ( var i=0;i<inputValue.length; i++){
+        if ( inputValue[i] == inputValue[i+1]){
+          //problem!!
+          setInputValue('');
+          alert("each digits must be different ");
+          return ;
+        }
+      }
+    }
     return (
       <div className='GameScreenDiv'>
         <Container>
@@ -136,28 +211,28 @@ function App() {
                   <p className='GuessNumber'>{guessnumber(4)}   {plusminus()}</p>
                 </Col>
               </Row>
+ 
               <div className='NumpadDiv'>
-                <div>
+                  <Button variant="outline-success" value = {0} onClick={handleClick}>0</Button>
+                  <Button variant="outline-success" value = {1} onClick={handleClick}>1</Button>
+                  <Button variant="outline-success" value = {2} onClick={handleClick}>2</Button>
+                  <Button variant="outline-success" value = {3} onClick={handleClick}>3</Button>
+                  <Button variant="outline-success" value = {4} onClick={handleClick}>4</Button>
+                  <Button variant="outline-success" value = {5} onClick={handleClick}>5</Button>
+                  <Button variant="outline-success" value = {6} onClick={handleClick}>6</Button>
+                  <Button variant="outline-success" value = {7} onClick={handleClick}>7</Button>
+                  <Button variant="outline-success" value = {8} onClick={handleClick}>8</Button>
+                  <Button variant="outline-success" value = {9} onClick={handleClick}>9</Button>
+                  <Button variant="outline-success" onClick={delBtn}> DEL</Button>   
+              </div>  
+              <form >
                   <input
-                    id="new-todo"
-                    value={inputValue}
-                    placeholder="Enter Your Guess"
-                    className='NumpadInput'
+                  onChange={handleChange}
+                  value={inputValue}
+                  placeholder="Enter Your Guess"
                   />
-                </div>
-                <Button variant="outline-success">0</Button>
-                <Button variant="outline-success">1</Button>
-                <Button variant="outline-success">2</Button>
-                <Button variant="outline-success">3</Button>
-                <Button variant="outline-success">4</Button>
-                <Button variant="outline-success">5</Button>
-                <Button variant="outline-success">6</Button>
-                <Button variant="outline-success">7</Button>
-                <Button variant="outline-success">8</Button>
-                <Button variant="outline-success">9</Button>
-                <Button variant="outline-success">DEL</Button>
-                <Button variant="outline-success">OK</Button>
-              </div>
+                  <Button variant="outline-success" onClick={() => [setIsGuessSet(1)]}>Enter</Button>
+              </form>
             </Col>
             <Col></Col>
           </Row>
@@ -165,6 +240,75 @@ function App() {
       </div>
     )
   }
+  function JoinMenu() {
+    const[roomCode, setRoomCode] = useState('')
+    function isSameRoomCode(){
+      if (roomCode == roomId)  {
+        setPage(3);
+         {/* ?? burara rooom id nasıl doğrulanacak??? */}
+
+      }
+
+
+    }
+    
+
+  
+ 
+    return (
+      <div className='JoinMenuDiv'>
+        
+        <form  className="RoomCodeInpt" >
+          <Col>
+          <label htmlFor='nme'> Username:  ‏‏‎‏‏‎ </label> 
+          <br></br>
+       
+
+
+          <input 
+            // type="text" 
+            // name="name"
+            // className="question" 
+            id="nme" 
+            required autoComplete="off" 
+            placeholder="Enter Your Name"
+            variant='dark'
+          />
+          
+          </Col>
+          <br>
+          
+          </br>
+            
+          <Col>
+          <label htmlFor='RooCode'> Room Code: </label> 
+          <br></br>
+         
+          <input 
+            // type="text" 
+            // name="name"
+           
+            id="RooCode" 
+            required autoComplete="off" 
+            placeholder="Enter Room Code"
+            value={roomCode}
+            onChange={(e) => setRoomCode(e.target.value)}/>
+           
+        
+          </Col>
+            <br>
+            
+            </br>
+            <Button variant='dark'  className='EnterBtn'   style={{ marginTop: 10 }} onClick={isSameRoomCode}>   {/*buraya onclick için function gelicek ve tıklayınca eğer roomCode = roomid =>  setPage(3) */}
+              Join Game 
+            </Button>
+        </form>
+      </div>
+    )
+
+
+  }
+    
 
 
 
@@ -175,7 +319,7 @@ function App() {
   else if (page == 1) {
     if (isUserCame == 0) {
       return (
-
+        // <GameScreen />
         <NewGameMenu />
 
       )
@@ -185,7 +329,17 @@ function App() {
         <GameScreen />)
     }
   }
+  else if (page == 2){
+    return(
+      <JoinMenu/>
+    )
+  }
+  else if (page == 3){
+    return (
+      <GameScreen />
+    )
 
+  }
 
 
 
